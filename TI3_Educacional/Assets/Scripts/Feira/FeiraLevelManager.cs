f685c55e05ce77a13ab2999c40d2611085173319
Gabriel Meyer <gmeyerp@gmail.com>
2024-09-24 21:12:35 -0300

    Criação de Cena com Frutas

---
+++ TI3_Educacional/Assets/Scripts/Feira/FeiraLevelManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeiraLevelManager : MonoBehaviour
+{
+    public static FeiraLevelManager instance;
+    float time;
+    float[] fruitTimer;
+    [SerializeField] int numberOfFruits = 3;
+    [SerializeField] List<GameObject> chosenFruits = new List<GameObject>();
+    [SerializeField] List<GameObject> fruits = new List<GameObject>();
+    [SerializeField] List<Stand> stands = new List<Stand>();
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        { instance = this; }
+        else { Destroy(gameObject); }
+
+    }
+
+    private void Start()
+    {
+        PickFruits();
+        GiveChosenFruits();
+        GiveRemainingFruits();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PickFruits()
+    {
+        for (int i = 0; i < numberOfFruits; i++)
+        {
+            int fruit = Random.Range(0, fruits.Count);
+            chosenFruits.Add(fruits[fruit]);
+            fruits.RemoveAt(fruit);
+        }       
+    }
+
+    public void GiveChosenFruits()
+    {
+        for (int i = 0; i < numberOfFruits; i++)
+        {
+            int s = Random.Range(0, stands.Count);
+            stands[s].PopulateStand(chosenFruits[i], i);
+            stands.RemoveAt(s);
+        }
+    }
+
+    public void GiveRemainingFruits()
+    {
+        foreach (Stand s in stands)
+        {
+            int fruit = Random.Range(0, fruits.Count);
+            if (!s.hasFruit)
+            {
+                s.PopulateStand(fruits[fruit]);
+            }
+        }
+    }
+}
---
+++ TI3_Educacional/Assets/Scripts/Feira/Stand.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stand : MonoBehaviour
+{
+    [SerializeField] Transform[] fruitSpot;
+    [SerializeField] GameObject interactSpot;
+    int chosenFruitIndex = -1;
+    public bool isChosen;
+    public bool hasFruit = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PopulateStand(GameObject fruit)
+    {
+        foreach (Transform spot in fruitSpot)
+        {
+            Instantiate(fruit, spot.position, fruit.transform.rotation, transform);
+        }
+        hasFruit = true;
+        interactSpot.SetActive(false);
+    }
+
+    public void PopulateStand(GameObject fruit, int chosenFruitIndex)
+    {
+        foreach (Transform spot in fruitSpot)
+        {
+            Instantiate(fruit, spot.position, fruit.transform.rotation, transform);
+        }
+        hasFruit = true;
+        isChosen = true;
+        this.chosenFruitIndex = chosenFruitIndex;
+        interactSpot.SetActive(true);
+    }
+}
